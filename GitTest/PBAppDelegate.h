40211a5300d60520048c218d6830034ee69ae48f
Vincent Le Normand <vincent@Pan.local>
2012-02-06 16:22:38 +0100

    Initial Commit

---
+++ GitTest/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  GitTest
+//
+//  Created by Vincent Le Normand on 06/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GitTest/PBAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  PBAppDelegate.m
+//  GitTest
+//
+//  Created by Vincent Le Normand on 06/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PBAppDelegate.h"
+
+@implementation PBAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
